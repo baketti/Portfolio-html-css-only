3b95cb45a7ae3821a69b6ec9d42a841ee3701ed0
baketti <ebachetti.11@gmail.com>
2024-02-06 09:13:33 +0100

    features-menu font-size fixed

--- assets/js/mobile_menu.js
+++ assets/js/mobile_menu.js
@@ -1,12 +1,32 @@
+/* 
+Con questo script gestiamo l'apertura e la chiusura del menu per dispositivi mobili: Tablet e mobile.
+Il menu veniva aperto e chiuso tramite l':hover, ma siccome nei dispositivi touch non esiste l'hover,
+perchè di fatto diventa un click, abbiamo dovuto gestire l'apertura e la chiusura del menu tramite javascript.
+Ora il menu può essere chiuso cliccando sul burger-menu,oppure cliccando in qualsiasi altro punto della finestra,
+lasciando invariato il comportamento precedente.
+Nei desktop in questo modo, il comportamento del css verrà sovrascritto
+e si potrà aprire e chiudere il menu sempre con un click.
+Una volta aperto, però, il css originale tornerà a funzionare normalmente, 
+e il menù potrà aprirsi e chiudersi con l'hover, dando di fatto al menu un comportamento ibrido.
+*/
+
 let menu = document.querySelector('.features-menu');
-let navbar = document.querySelector('.navbar');
 let burgerMenu = document.querySelector('.burger-menu');
 let isMenuOpen = false;
 
+/*
+Aggiungiamo subito la proprietà sovrascrivendo il css originale, 
+altrimenti non funzionerebbe al primo click, inizierebbe a funzionare solo al secondo
+*/
 window.onload = () => {
     menu.style.display = 'none';
 }
-
+/* 
+Con questa funzione, al click sul burger menu:
+    1)se il menu è aperto lo chiudiamo, altrimenti lo apriamo 
+    2)una volta aperto togliamo la proprietà display: none, 
+      e la impostiamo a vuota, lasciando la gestione al css originale
+*/
 function toggleMenu() {
     if (isMenuOpen) {
         menu.style.display = 'none';
@@ -16,19 +36,26 @@ function toggleMenu() {
         isMenuOpen = true;
     }
 }
-
+/* Assegnamo l'evento al burger-menu */
 burgerMenu.addEventListener('click', (event) => {
     event.stopPropagation();
     toggleMenu();
 });
-
+/* 
+Aggiungiamo l'evento all'intera finestra, 
+facendo di fatto un override della gestione dell':hover 
+*/
 window.addEventListener('click', () => {
     if (isMenuOpen) {
         menu.style.display = 'none';
         isMenuOpen = false;
     }
 });
-
+/* 
+Appena la dimensione della finestra è tale da mostare il burger menu
+assegnamo la proprietà display:none al menu,
+altrimenti inizierebbe a funzionare solo al secondo click
+*/
 window.addEventListener('resize', () => {
     if (window.innerWidth <= 1060) {
         menu.style.display = 'none'; 
